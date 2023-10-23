699040d363c555612f021347a7d30162af09527e
Miruzbek <82485402+Miruzbek@users.noreply.github.com>
2023-10-23 20:33:28 +0300

    Add project files.

---
+++ 01_TypesAndVariables/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _01_TypesAndVariables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            // Value Types 
+            int number1 = -2147483648;
+            int number2= 2147483647;
+            Console.WriteLine("Number 1 is {0} ",number1);
+        }
+    }
+}
