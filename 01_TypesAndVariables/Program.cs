093a5368a981956981efd4582c82eea7a0e14a79
Miruzbek <82485402+Miruzbek@users.noreply.github.com>
2023-10-24 21:07:11 +0300

    Decimal variable type added

--- 01_TypesAndVariables/Program.cs
+++ 01_TypesAndVariables/Program.cs
@@ -7,6 +7,7 @@
             Console.WriteLine("Hello, World!");
             // Value Types
             double double_ = 10.4;
+            decimal number5 = 10;
             char character = 'A';
             bool condition = false;
             byte byteNumber = 255;
@@ -21,8 +22,8 @@
             Console.WriteLine("Number 2 is {0} ",number);
             Console.WriteLine("Number 3 is {0} ",number1);
             Console.WriteLine("Number 4 is {0} ", number2);
-            Console.WriteLine("Number 5 is {0} ", number2);
-            Console.WriteLine("Number 6 is {0} ", number2);
+            Console.WriteLine("Number 5 is {0} ", number3);
+            Console.WriteLine("Number 6 is {0} ", number4);
             Console.WriteLine("Number 7 is {0} ", double_);
             Console.WriteLine("Character is {0} ", character);
 
