eefe24a69bb15b255fdbcc3900bf3a12a7dab0c7
Miruzbek <82485402+Miruzbek@users.noreply.github.com>
2023-10-24 20:55:13 +0300

    Byte variabl type added

--- 01_TypesAndVariables/Program.cs
+++ 01_TypesAndVariables/Program.cs
@@ -5,15 +5,22 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            // Value Types 
+            // Value Types
+            byte byteNumber = 255;
+
             short number = 32767;
             short number_ = -32768;
             int number1 = -2147483648;
             int number2= 2147483647;
             long number3 = 9223372036854775807;
             long number4 = -9223372036854775808;
-
-            Console.WriteLine("Number 1 is {0} ",number1);
+            Console.WriteLine("Number  is {0} ", byteNumber);
+            Console.WriteLine("Number 1 is {0} ", number_);
+            Console.WriteLine("Number 2 is {0} ",number);
+            Console.WriteLine("Number 3 is {0} ",number1);
+            Console.WriteLine("Number 4 is {0} ", number2);
+            Console.WriteLine("Number 5 is {0} ", number2);
+            Console.WriteLine("Number 6 is {0} ", number2);
         }
     }
 }
