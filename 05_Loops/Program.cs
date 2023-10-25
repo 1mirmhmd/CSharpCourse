d991cc1586afe98e832bf9d7564d4c6bf99a3efb
Miruzbek <82485402+Miruzbek@users.noreply.github.com>
2023-10-25 08:17:41 +0300

    For and While Loop examples added

---
+++ 05_Loops/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _05_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 100; i >=0; i=i-2)
+            {
+                Console.WriteLine(i);
+            }
+            int number = 100;
+            while (number>=0)
+            {
+                Console.WriteLine(number);
+                number--;
+            }
+        }
+    }
+}
