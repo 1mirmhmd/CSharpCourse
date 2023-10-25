fff2345b3a6ffd6520c4555bb281b3f0f751e4df
Miruzbek <82485402+Miruzbek@users.noreply.github.com>
2023-10-25 08:10:00 +0300

    Array examples added

--- 03_Methods/Program.cs
+++ 03_Methods/Program.cs
@@ -8,33 +8,38 @@
             int number1 = 20;
             int number2 = 100;
             // ref ve out keywordu referans tanımlamak için kullanılır. ref kullanıldığında değişkende değer tanımlanmalı, out'ta ise tanımlamaya gerek yok.
-            var result1 =Add2( ref number1 , number2);
+            var result1 = Add2(ref number1, number2);
             var result = Add1(120, 120);
             Console.WriteLine(result1);
+            Console.WriteLine(Add3(1, 2, 3, 4, 5));
         }
         static void Add()
         {
             Console.WriteLine("Added");
         }
-        static int Add1 (int number1=20, int number2=10)
+        static int Add1(int number1 = 20, int number2 = 10)
         {
-            var result = number1+number2;
+            var result = number1 + number2;
             return result;
         }
         static int Add2(ref int number1, int number2)
         {
-            return number1+number2;
+            return number1 + number2;
         }
 
         // Method overloading
         static int Multiply(int number1, int number2)
         {
-            return number1*number2;
+            return number1 * number2;
         }
 
         static int Multiply(int number1, int number2, int number3)
         {
-            return number1 * number2*number3;
+            return number1 * number2 * number3;
+        }
+        static int Add3( int number,params int[] numbers) // Tekli parametre her zaman en önde eklenmeli
+        {
+            return numbers.Sum();
         }
     }
 }
---
+++ 04_Arrays/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _04_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] students = new string[3] { "Ali", "Müslmüm", "Nadir" };
+            string[] students1 = { "Hilal", "Derin", "Sıla" };
+            foreach (var student in students1)
+            {
+                Console.WriteLine(student);
+            }
+            string[,] regions = new string[4, 2] {
+                {"İstanbul","İzmit" },
+                {"Ankara","Konya" },
+                {"Antalya","Adana" },
+                {"Rize","Trabzon" } 
+            };
+
+            for (int i = 0; i <= regions.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= regions.GetUpperBound(1); j++)
+                {
+                    Console.WriteLine(regions[i,j]);
+                }
+                Console.WriteLine("**********");
+            }
+        }
+    }
+}
