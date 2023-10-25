07d4d1f39da8890e17df1d96289635f863a6fa30
Miruzbek <82485402+Miruzbek@users.noreply.github.com>
2023-10-25 08:23:12 +0300

    Do while loop added

--- 05_Loops/Program.cs
+++ 05_Loops/Program.cs
@@ -4,15 +4,31 @@
     {
         static void Main(string[] args)
         {
-            for (int i = 100; i >=0; i=i-2)
-            {
-                Console.WriteLine(i);
-            }
-            int number = 100;
-            while (number>=0)
+            //ForLoop();
+            //WhileLoop();
+            int number = 10;
+            do
             {
                 Console.WriteLine(number);
                 number--;
+            } while (number>=0);
+        }
+
+        private static void WhileLoop()
+        {
+            int number = 100;
+            while (number >= 0)
+            {
+                Console.WriteLine(number);
+                number--;
+            }
+        }
+
+        private static void ForLoop()
+        {
+            for (int i = 100; i >= 0; i = i - 2)
+            {
+                Console.WriteLine(i);
             }
         }
     }
