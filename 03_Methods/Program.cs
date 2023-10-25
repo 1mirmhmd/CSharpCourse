4ded55f5592c0cfae59369e53fe2578d3043b5eb
Miruzbek <82485402+Miruzbek@users.noreply.github.com>
2023-10-25 07:35:54 +0300

    Methods example added

---
+++ 03_Methods/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _03_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Add();
+            var result = Add1(120, 120);
+            Console.WriteLine(result);
+        }
+        static void Add()
+        {
+            Console.WriteLine("Added");
+        }
+        static int Add1 (int number1=20, int number2=10)
+        {
+            var result = number1+number2;
+            return result;
+        }
+    }
+}
