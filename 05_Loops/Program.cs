d75afa15ab4982d37cbd6354ee3f9ccfb035e3dd
Miruzbek <82485402+Miruzbek@users.noreply.github.com>
2023-10-25 08:43:14 +0300

    String intro added

--- 05_Loops/Program.cs
+++ 05_Loops/Program.cs
@@ -8,6 +8,28 @@
             //WhileLoop();
             //DoWhileLoop();
             //ForEachLoop();
+            if (IsPrimeNumber(6))
+            {
+                Console.WriteLine("This is a prime number");
+            }
+            else
+            {
+                Console.WriteLine("This is not a prime number");
+            }
+
+        }
+        private static bool IsPrimeNumber(int number)
+        {
+            bool result = true;
+            for (int i = 2; i < number - 1; i++)
+            {
+                if (number % i == 0)
+                {
+                    result = false;
+                    i = number;
+                }
+            }
+            return result;
         }
 
         private static void ForEachLoop()
---
+++ 06_Strings/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _06_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //StringIntro();
+            string sentence = "My name is 1mirmhmd";
+        }
+
+        private static void StringIntro()
+        {
+            string city = "Ankara";
+            Console.WriteLine(city[0]);
+            foreach (var item in city)
+            {
+                Console.WriteLine(item);
+            }
+            string city2 = "İstanbul";
+            //string result = city + city2;
+            //Best Practice
+
+            Console.WriteLine(String.Format("{0} {1}", city, city2));
+        }
+    }
+}
