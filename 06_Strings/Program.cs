60ca03916f85d6a6821cfabe88beb58cf8a27cf1
Miruzbek <82485402+Miruzbek@users.noreply.github.com>
2023-10-25 08:57:05 +0300

    String Methods example added

--- 06_Strings/Program.cs
+++ 06_Strings/Program.cs
@@ -6,6 +6,23 @@
         {
             //StringIntro();
             string sentence = "My name is 1mirmhmd";
+            var result = sentence.Length;
+            var resul1 = sentence.Clone();
+
+            sentence = "My name is mirmhmd";
+
+            bool result2 = sentence.EndsWith("b");
+            bool result3 = sentence.StartsWith("My name");
+
+            var result4 = sentence.IndexOf("name");
+            var result5 = sentence.LastIndexOf(" ");
+            var result6 = sentence.Insert(0, "Hello, ");
+            var result7 = sentence.Substring(3, 4);
+            var result8 = sentence.ToUpper();
+            var result9 = sentence.ToLower();
+            var result10 = sentence.Replace(" ", "-");
+            var result11 = sentence.Remove(2, 4);
+            Console.WriteLine(result11);
         }
 
         private static void StringIntro()
