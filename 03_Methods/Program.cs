9f1cd153f72ee14d60d5436f48d97159b489d33f
Miruzbek <82485402+Miruzbek@users.noreply.github.com>
2023-10-25 07:47:55 +0300

    Ref and out keyword example added

--- 03_Methods/Program.cs
+++ 03_Methods/Program.cs
@@ -5,8 +5,12 @@
         static void Main(string[] args)
         {
             Add();
+            int number1 = 20;
+            int number2 = 100;
+            // ref ve out keywordu referans tanımlamak için kullanılır. ref kullanıldığında değişkende değer tanımlanmalı, out'ta ise tanımlamaya gerek yok.
+            var result1 =Add2( ref number1 , number2);
             var result = Add1(120, 120);
-            Console.WriteLine(result);
+            Console.WriteLine(result1);
         }
         static void Add()
         {
@@ -16,6 +20,10 @@
         {
             var result = number1+number2;
             return result;
+        }
+        static int Add2(ref int number1, int number2)
+        {
+            return number1+number2;
         }
     }
 }
