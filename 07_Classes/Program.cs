eb610096db8ebc4a15609306f1a00d9c29e46cd9
1mirmhmd <mirmhmdmahmud@gmail.com>
2023-10-27 08:23:05 +0300

    Class example added

---
+++ 07_Classes/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _07_Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add();
+            ProductManager productManager = new ProductManager();
+            productManager.Add();
+            productManager.Update();
+            Customer customer = new Customer();
+            customer.Id = 1;
+            customer.FirstName = "Halil";
+            customer.LastName = "Can";
+            customer.City = "Sivas";
+            Customer customer1 = new Customer { Id = 2, FirstName = "Murat", LastName = "Kulu", City = "Ordu" };
+            Console.WriteLine(customer1.FirstName);
+        }
+    }
+
+
+}
